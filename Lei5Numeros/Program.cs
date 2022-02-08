ece63a1afda4f2299b206985a66e224ecedf71ad
Anderson Andrade da Silva <andersonperf@yahoo.com.br>
2022-02-08 13:40:43 -0300

    Desafio
    Desafio

---
+++ Lei5Numeros/Program.cs
@@ -0,0 +1,75 @@
+﻿
+int Numero1 = 10;
+int Numero2 = 20;
+int Numero3 = 30;
+int Numero4 = 40;
+int Numero5 = 50;
+
+
+Console.WriteLine("O Numero 1 é:" + Numero1);
+Console.WriteLine("O Numero 2 é:" + Numero2);
+Console.WriteLine("O Numero 3 é:" + Numero3);
+Console.WriteLine("O Numero 4 é:" + Numero4);
+Console.WriteLine("O Numero 5 é:" + Numero5);
+
+    
+  
+    {  
+        int[] numeros = {  
+            10,  
+            20,  
+            30,  
+            40,  
+            50,    
+        };  
+        var intMenorNumero = numeros.Min();  
+        Console.WriteLine("O Menor Numero é {0}", intMenorNumero);  
+        var intMaiorNumero = numeros.Max();  
+        Console.WriteLine("O Maior Numero é {0}", intMaiorNumero);  
+
+        
+
+         int soma = (Numero1 + Numero2 + Numero3 + Numero4 + Numero5);
+
+         int media; media = soma / 5;
+
+               
+            Console.WriteLine(" A media é: " + media);
+            Console.WriteLine(" ");
+
+            //while ( Numero1 == Numero2)
+           // {
+              // Console.WriteLine(Numero1);
+                    // break;}
+
+            //while ( Numero2 == Numero3)
+           // {
+              // Console.WriteLine(Numero2);
+                    // break;}
+
+            //while ( Numero3 == Numero4)
+            //{
+               //Console.WriteLine(Numero3);
+                    // break;}
+
+           // while ( Numero4 == Numero5)
+            //{
+              // Console.WriteLine(Numero4);
+                    // break;}   
+             //Console.WriteLine((Numeros: Numero1 + Numero2 + Numero3 + Numero4 + Numero5 + (" São diferentes: "));        
+
+                  
+             Boolean compara =  10 == 20;  
+             Boolean compara1 = 20 == 30;
+             Boolean compara2 = 30 == 40;
+             Boolean compara3 = 40 == 50;
+
+             Console.WriteLine(" 10  = 20 ?: " + compara);
+             Console.WriteLine(" 20  = 30 ?: " + compara1);
+             Console.WriteLine(" 30  = 40 ?: " + compara2);
+             Console.WriteLine(" 40  = 50 ?: " + compara3);             
+                        
+       
+    Console.ReadLine();  
+    } 
+    
